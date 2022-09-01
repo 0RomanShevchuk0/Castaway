ab27d99fe88396d8e307d08208e9485aa664f30b
0cham0m1le0 <107765648+0cham0m1le0@users.noreply.github.com>
2022-09-01 17:19:36 +0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,4 @@
+//*Бургер
 document.querySelector('#navbar__burger').addEventListener('click', function() {
 	this.classList.toggle('active');
 	document.querySelector('#navbar__menu').classList.toggle('active');
@@ -5,3 +6,33 @@ document.querySelector('#navbar__burger').addEventListener('click', function() {
 })
 
 document.querySelector('#navbar__logo').addEventListener('click', () => location.reload())
+
+
+//*Рейтинги
+const ratings = document.querySelectorAll('.rating');
+if (ratings.length > 0) {
+	initRatings();
+}
+
+function initRatings() {
+	let ratingActive, ratingValue;
+	for (let index = 0; index < ratings.length; index++) {
+		const rating = ratings[index];
+		initRating(rating);
+	}
+
+	function initRating(rating) {
+		initRatingVars(rating);
+		setRatingActiveWidth();
+	}
+
+	function initRatingVars(rating) {
+		ratingActive = rating.querySelector('.rating__active')
+		ratingValue = rating.querySelector('.rating__value')
+	}
+
+	function setRatingActiveWidth(index = ratingValue.innerHTML) {
+		const ratingActiveWidth = index / 0.05;
+		ratingActive.style.width = `${ratingActiveWidth}%`;
+	}
+}
